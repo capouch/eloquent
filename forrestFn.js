28602578ac32a64a5030c16785004fa92394aa1f
Brian Capouch <brianc@palaver.net>
2017-07-02 14:42:53 -0400

    Fix "undefined" return value issue

---
+++ forrestFn.js
@@ -0,0 +1,19 @@
+function countChar(s, c, x){
+  let temp
+  var count = x;
+  if (s.length == 0)
+
+    return count
+
+  //  Increment count when c matches the last char of string
+  if ((s.charAt(s.length - 1)) === c){
+    count++;
+    var substring = s.substring(0, s.length-1);
+    return countChar(s.substring(0,s.length-1), c, count);
+  }
+  else{
+    // Move "end of string" one position and go again
+    return countChar(s.substring(0,s.length-1), c, count);
+  }
+}
+console.log(countChar("kakkasdfaskasdakk", "k", 0));

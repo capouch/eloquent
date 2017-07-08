493186781afd91d6e8cff1d6ff6ed9b484e1445a
Brian Capouch <brianc@palaver.net>
2017-07-08 00:05:43 -0400

    Weresquirrel code from Chapter 4

---
+++ analyzeData.js
@@ -0,0 +1,49 @@
+let JOURNAL = require('./JOURNAL')
+
+function hasEvent(event, entry) {
+  return entry.events.indexOf(event) != -1;
+}
+
+function tableFor(event, journal) {
+  var table = [0, 0, 0, 0];
+  for (var i = 0; i < journal.length; i++) {
+    var entry = journal[i], index = 0;
+    if (hasEvent(event, entry)) index += 1;
+    if (entry.squirrel) index += 2;
+    table[index] += 1;
+  }
+  return table;
+}
+
+function phi(table) {
+  return (table[3] * table[0] - table[2] * table[1]) /
+    Math.sqrt((table[2] + table[3]) *
+              (table[0] + table[1]) *
+              (table[1] + table[3]) *
+              (table[0] + table[2]));
+}
+
+function gatherCorrelations(journal) {
+  var phis = {};
+  for (var entry = 0; entry < journal.length; entry++) {
+    var events = journal[entry].events;
+    for (var i = 0; i < events.length; i++) {
+      var event = events[i];
+      if (!(event in phis))
+        phis[event] = phi(tableFor(event, journal));
+    }
+  }
+  return phis;
+}
+
+// var correlations = gatherCorrelations(JOURNAL);
+// console.log('All correlations:' + JSON.stringify(correlations) + '\n')
+// console.log(correlations.pizza);
+
+for (var i = 0; i < JOURNAL.length; i++) {
+  var entry = JOURNAL[i];
+  if (hasEvent("peanuts", entry) &&
+     !hasEvent("brushed teeth", entry))
+    entry.events.push("peanut teeth");
+}
+console.log(phi(tableFor("peanut teeth", JOURNAL)));

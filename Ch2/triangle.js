020047e2f2991b9f6b2e0f00f1b7c200ca18f840
capouch <brianc@palaver.net>
2020-12-17 06:26:36 -0500

    Show another way of solving this, add some comments, and clean up code

--- Ch2/triangle.js
+++ Ch2/triangle.js
@@ -3,9 +3,23 @@
   Brian Capouch 12/15/20
   */
 
-let rowMax = 10,
-  stringVal = "#"
-  for (let counter = 0; counter < rowMax; counter++) {
+const ROWMAX = 7 // What book does
+let stringVal = "#"
+  // Custom is to use simple letters i, j, k etc. for loop indices
+  for (let i = 0; i < ROWMAX; i++) {
     console.log(stringVal);
     stringVal += "#";
   }
+
+  /* Here's an alternative way to do it, demonstrating the hint
+     given in the text, and also using a do() construct
+  */
+
+  // Constant and variable are already defined up above
+  
+  // Reset stringVal to starting value
+  stringVal = "#"
+  do {
+    console.log(stringVal)
+    stringVal += "#"
+  } while (stringVal.length <= ROWMAX)

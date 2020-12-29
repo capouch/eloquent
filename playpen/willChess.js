9facde23a0d108a6ff5319b72a09706609d05c90
capouch <brianc@palaver.net>
2020-12-28 23:51:51 -0500

    Clean up, add comments, constantify former variables because they never change values

--- playpen/willChess.js
+++ playpen/willChess.js
@@ -1,22 +1,33 @@
-let ROWMAX = 8
-let COLMAX = 8
-let b = '#'
-let w = '-';
+/* Modify Will's code from mammoth debug session to make it work
+  Brian Capouch 12/28/20
+  */
+
+// Make these constants, as they will not change over the course of the
+//  program's run.  By convention, CONSTANTS ARE ALL CAPS
+const ROWMAX = 8,
+ COLMAX = 8,
+ B = '#',
+ W = '-';
+
+// This one, however, MUST be a variable; it's value will change
 let stringVal = "" // Safety first; set it to empty string
 
+// Outer loop generates ROWMAX lines
 for (let i = 0; i < ROWMAX; i++) {
  // console.log("In outer for on pass " + i)
+
+ // Inner loop generates COLMAX columns of a single line
  for (let j = 0; j < COLMAX; j ++) {
   // console.log("In inner for on pass " + j)
   if (i % 2 == 0) {
     // console.log("I am in if part of test")
     // stringVal = (w += b);
-    stringVal += (j % 2 == 0?b:w)
+    stringVal += (j % 2 == 0? B : W)
     // console.log("Even building stringVal: " + stringVal)
   } else {
     // console.log("I am in else part of test")
     // stringVal = (b += w);
-    stringVal += (j % 2 == 0?w:b)
+    stringVal += (j % 2 == 0? W : B)
     // console.log("Odd building stringVal: " + stringVal)
     }
   }

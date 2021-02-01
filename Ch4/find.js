ca415c8635b57ba647fc4af13b76c1b127e12eb6
capouch <brianc@palaver.net>
2021-02-01 00:30:17 -0500

    Added recursive version to find()

--- Ch4/find.js
+++ Ch4/find.js
@@ -1,25 +1,48 @@
-// require("./arrayToList.js")
-// require("./prepend.js")
+// These lines needed for testing
+//require("./arrayToList.js")
+//require("./prepend.js")
 
+/* find: Look for a given value in a list object
+   Riff on list exercise from Ch. 4 of Eloquent
+   Brian Capouch 31 January 2021
+*/
+
+/*
+// Iterative version
 const find = (searchValue, list) => {
-  // This is an on-purpose infinite loop
-  // Note use of break statement to get out of it
+
+  // Iterate over entries looking for match
   while (list != null) {
     if (list.value == searchValue)
       return list
     else
       list = list.rest
     }
+
+    // Note it will return null if no match
     return list
   }
+*/
+
+
+// Recursive version
+const find = (searchValue, list) => {
+  if (list == null)
+    return list
+  else
+    if (list.value == searchValue)
+      return list
+    else
+      return find(searchValue, list.rest)
+}
 
 
 
-
-/* Here is my test scaffolding
+/*
+// Here is my test scaffolding
 let list = arrayToList([10, 20, 30, 40, 50, 60])
 // Let's see if we can find that 40
-console.log(find(5, list))
+console.log(find(30, list))
 */
 
 

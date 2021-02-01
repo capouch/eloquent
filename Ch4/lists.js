2cc49b159953adf2c4bee538d47a6d1d63e343c3
capouch <brianc@palaver.net>
2021-02-01 00:19:12 -0500

    Oops needed to comment out "output" of find

--- Ch4/lists.js
+++ Ch4/lists.js
@@ -18,7 +18,7 @@ require ('./find.js')
 console.log(arrayToList([10, 20, 30, 40, 50, 60]));
 // → {value: 10, rest: {value: 20, rest: null}}
 console.log(find(30, arrayToList([10, 20, 30, 40, 50, 60])))
-{ value: 30, rest: { value: 40, rest: { value: 50, rest: {value: 60, rest: null} } } }
+// { value: 30, rest: { value: 40, rest: { value: 50, rest: {value: 60, rest: null} } } }
 console.log(listToArray(arrayToList([10, 20, 30])));
 // → [10, 20, 30]
 console.log(prepend(10, prepend(20, null)));
